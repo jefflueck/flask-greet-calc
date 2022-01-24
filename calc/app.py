d101ce2c9eec8c933777aa47aefa6e91421bd784
jefflueck <lueckcfd@yahoo.com>
2022-01-24 13:51:23 -0600

    Finished exercise code

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,38 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+@app.route('/add')
+def do_add():
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+
+  result = a + b
+  return f"<h1>The result of {a} &#43 {b} is &#58; {result}</h1>"
+
+@app.route('/sub')
+def do_sub():
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+
+  result = b - a
+  return f"<h1>The result of {b} &#8722; {a} is &#58; {result}</h1>"
+
+@app.route('/mult')
+def do_mult():
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+
+  result = a * b
+  return f"<h1>The result of {a} &#215; {b} is &#58; {result}</h1>"
+
+
+@app.route('/div')
+def do_div():
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+
+  result = a / b
+  return f"<h1>The result of {a} &divide {b} is &#58; {result}</h1>"
---
+++ greet/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+  return f"<h1>Welcome</h1>"
+
+@app.route('/welcome/home')
+def welcome_home():
+  return f"<h1>Welcome Home</h1>"
+
+@app.route('/welcome/back')
+def welcome_back():
+  return f"<h1>Welcome Back</h1>"
